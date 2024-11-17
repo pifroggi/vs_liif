b1a752cc5313a27eaf5cfea78163d6373cdf21bf
pifroggi <pifroggimc@gmail.com>
2024-11-17 18:35:09 +0100

    src_left/src_top now act like in vs build-in resizers, added src_width/src_height, performance improvements

--- vs_liif/utils.py
+++ vs_liif/utils.py
@@ -1,22 +1,24 @@
 import torch
 
-def make_coord(shape, ranges=None, flatten=True, src_left=0.0, src_top=0.0):
-    """ Make coordinates at grid centers with optional subpixel shifts.
-    """
+def make_coord(shape, ranges=None, flatten=True):
+    """ Make coordinates at grid centers """
+    if ranges is None:
+        ranges = [(-1, 1), (-1, 1)]
     coord_seqs = []
     for i, n in enumerate(shape):
-        if ranges is None:
-            v0, v1 = -1, 1
-        else:
-            v0, v1 = ranges[i]
+        v0, v1 = ranges[i]
         r = (v1 - v0) / (2 * n)
-        if i == 0:
-            shift = src_top * (v1 - v0) / n
-        else:
-            shift = src_left * (v1 - v0) / n
-        seq = v0 + r + shift + (2 * r) * torch.arange(n).float()
+        seq = v0 + r + (2 * r) * torch.arange(n).float()
         coord_seqs.append(seq)
     ret = torch.stack(torch.meshgrid(*coord_seqs, indexing='ij'), dim=-1)
     if flatten:
         ret = ret.view(-1, ret.shape[-1])
     return ret
+
+def mirror_coord(coord, vmin=-1.0, vmax=1.0):
+    """ Mirror out of bounds areas """
+    range_size = vmax - vmin
+    coord = (coord - vmin) % (2 * range_size)
+    coord = torch.where(coord > range_size, 2 * range_size - coord, coord)
+    coord = coord + vmin
+    return coord
